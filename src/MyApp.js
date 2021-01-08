512b740006e0657cb954918cfbb74c4f8a1f41a4
Jerbilcraft <jburiane@calpoly.edu>
2021-01-08 13:07:30 -0800

    added form and submit functionality

--- src/MyApp.js
+++ src/MyApp.js
@@ -1,14 +1,13 @@
 import React, {useState} from 'react';
 import Table from './Table';
+import Form from './Form';
 
 function MyApp() {
-    const [characters, setCharacters] = useState([
-        {
-            name: 'Charlie',
-            job: 'Janitor',
-            // the rest of the data
-        },
-    ]);
+    const [characters, setCharacters] = useState([]);
+
+    function updateList(person) {
+        setCharacters([...characters, person]);
+    }
 
     function removeOneCharacter(index) {
         const updated = characters.filter((character, i) => {
@@ -20,8 +19,9 @@ function MyApp() {
     return (
         <div className="container">
             <Table characterData={characters} removeCharacter={removeOneCharacter} />
+            <Form handleSubmit={updateList} />
         </div>
-    ) 
+    )
 }
 
 export default MyApp;
