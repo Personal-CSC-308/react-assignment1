c2ff6b1de564dc29bd80b28c4775aea0ad238e90
Jerbilcraft <jburiane@calpoly.edu>
2021-01-14 19:45:29 -0800

    end of part 4

--- src/MyApp.js
+++ src/MyApp.js
@@ -1,9 +1,25 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import Table from './Table';
 import Form from './Form';
+import axios from 'axios';
+
 
 function MyApp() {
     const [characters, setCharacters] = useState([]);
+
+    useEffect(() => {
+        fetchAll().then( result => {
+           if (result)
+              setCharacters(result);
+         });
+     }, [] );
+
+    return (
+        <div className="container">
+            <Table characterData={characters} removeCharacter={removeOneCharacter} />
+            <Form handleSubmit={updateList} />
+        </div>
+    )
 
     function updateList(person) {
         setCharacters([...characters, person]);
@@ -16,12 +32,18 @@ function MyApp() {
         setCharacters(updated);
     }
 
-    return (
-        <div className="container">
-            <Table characterData={characters} removeCharacter={removeOneCharacter} />
-            <Form handleSubmit={updateList} />
-        </div>
-    )
+    async function fetchAll(){
+        try {
+           const response = await axios.get('http://localhost:5000/users');
+           return response.data.users_list;     
+        }
+        catch (error){
+           //We're not handling errors. Just logging into the console.
+           console.log(error); 
+           return false;         
+        }
+     }
+
 }
 
 export default MyApp;
